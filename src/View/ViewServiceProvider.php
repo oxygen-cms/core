8d030864415534411e07610f9000ae8408e47d26
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-13 20:59:18 +1000

    Began porting to Laravel 5

--- src/View/ViewServiceProvider.php
+++ src/View/ViewServiceProvider.php
@@ -3,6 +3,10 @@
 namespace Oxygen\Core\View;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Engines\PhpEngine;
+use Illuminate\View\FileViewFinder;
 
 class ViewServiceProvider extends ServiceProvider {
 
@@ -12,9 +16,64 @@ class ViewServiceProvider extends ServiceProvider {
      * @return void
      */
     public function register() {
-        $this->registerDbBladeEngine($this->app['view.engine.resolver']);
+        $this->registerEngineResolver();
+
+        $this->registerViewFinder();
 
         $this->registerFactory();
+    }
+
+    /**
+     * Register the engine resolver instance.
+     *
+     * @return void
+     */
+    public function registerEngineResolver() {
+        $this->app->singleton('view.engine.resolver', function () {
+            $resolver = new EngineResolver;
+
+            // Next we will register the various engines with the resolver so that the
+            // environment can resolve the engines it needs for various views based
+            // on the extension of view files. We call a method for each engines.
+            foreach (['php', 'blade', 'bladeString'] as $engine) {
+                $this->{'register'.ucfirst($engine).'Engine'}($resolver);
+            }
+
+            return $resolver;
+        });
+    }
+
+    /**
+     * Register the PHP engine implementation.
+     *
+     * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+     * @return void
+     */
+    public function registerPhpEngine($resolver) {
+        $resolver->register('php', function () { return new PhpEngine; });
+    }
+
+    /**
+     * Register the Blade engine implementation.
+     *
+     * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+     * @return void
+     */
+    public function registerBladeEngine($resolver) {
+        $app = $this->app;
+
+        // The Compiler engine requires an instance of the CompilerInterface, which in
+        // this case will be the Blade compiler, so we'll first create the compiler
+        // instance to pass into the engine so it can compile the views properly.
+        $app->singleton('blade.compiler', function ($app) {
+            $cache = $app['config']['view.compiled'];
+
+            return new BladeCompiler($app['files'], $cache);
+        });
+
+        $resolver->register('blade', function () use ($app) {
+            return new CompilerEngine($app['blade.compiler'], $app['files']);
+        });
     }
 
     /**
@@ -23,7 +82,7 @@ class ViewServiceProvider extends ServiceProvider {
      * @param  \Illuminate\View\Engines\EngineResolver  $resolver
      * @return void
      */
-    public function registerDbBladeEngine($resolver) {
+    public function registerBladeStringEngine($resolver) {
         $app = $this->app;
 
         // The Compiler engine requires an instance of the CompilerInterface, which in
@@ -42,12 +101,25 @@ class ViewServiceProvider extends ServiceProvider {
     }
 
     /**
+     * Register the view finder implementation.
+     *
+     * @return void
+     */
+    public function registerViewFinder() {
+        $this->app->bind('view.finder', function ($app) {
+            $paths = $app['config']['view.paths'];
+
+            return new FileViewFinder($app['files'], $paths);
+        });
+    }
+
+    /**
      * Register the view environment.
      *
      * @return void
      */
     public function registerFactory() {
-        $this->app->bindShared('view', function($app) {
+        $this->app->singleton('view', function ($app) {
             // Next we need to grab the engine resolver instance that will be used by the
             // environment. The resolver will be used by an environment to get each of
             // the various engine implementations such as plain PHP or Blade engine.
@@ -67,5 +139,4 @@ class ViewServiceProvider extends ServiceProvider {
             return $env;
         });
     }
-
 }
