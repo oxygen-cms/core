f12321a47e33dfd7c515598469ae40d3156bb005
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-26 12:42:20 +1000

    Fixed service provider

--- src/Translation/TranslationServiceProvider.php
+++ src/Translation/TranslationServiceProvider.php
@@ -49,7 +49,7 @@ class TranslationServiceProvider extends ServiceProvider {
      */
     protected function registerLoader() {
         $this->app->singleton('translation.loader', function($app) {
-            return new FileLoader($app['files'], $app['path'].'/lang');
+            return new FileLoader($app['files'], $app['path.lang']);
         });
     }
 
