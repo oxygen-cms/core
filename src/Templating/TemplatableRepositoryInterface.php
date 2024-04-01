fba39abd397727370d668d18db3c235301548a94
Chris Chamberlain <chris@chamberlain.id.au>
2024-04-01 20:23:28 +1100

    add extra type annotations

--- src/Templating/TemplatableRepositoryInterface.php
+++ src/Templating/TemplatableRepositoryInterface.php
@@ -13,6 +13,6 @@ interface TemplatableRepositoryInterface extends RepositoryInterface {
      * @param bool $onlyPublished whether to only consider published items when templating
      * @return Templatable|null
      */
-    public function findByTemplateKey($key, $onlyPublished = true);
+    public function findByTemplateKey(string $key, bool $onlyPublished = true): ?Templatable;
 
 }
